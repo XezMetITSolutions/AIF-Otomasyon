3a978ecdaeaa152cd28263fbd1d0d2a45c0f9fc9
XezMet <office@xezmet.at>
2026-02-05 20:32:41 +0100

    Fix: incorrect include paths in add-missing-users.php

--- database/add-missing-users.php
+++ database/add-missing-users.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/includes/init.php';
-require_once __DIR__ . '/classes/Database.php';
+require_once __DIR__ . '/../includes/init.php';
+require_once __DIR__ . '/../classes/Database.php';
 
 $db = Database::getInstance();
 
